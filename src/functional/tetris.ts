c0fa216b1c70e96494b8204d8d2df46cc8c8a6bd
Folkert Niewijk <fniewijk@github.com>
2020-05-06 20:55:10 +0200

    working on the game

--- src/functional/tetris.ts
+++ src/functional/tetris.ts
@@ -6,8 +6,6 @@ export interface Output {
 }
 
 export interface Input extends Output {
-  field: Field[][];
-  piece: TetrominoPiece;
   move?: Move;
   rotate?: boolean;
   nextPiece: TetrominoPiece;
@@ -16,14 +14,16 @@ export interface Input extends Output {
 const pieces = {
   iPiece: { x: 0, y: 0, shape: [[1, 1], [1, 1]] },
   oPiece: { x: 0, y: 0, shape: [[1], [1], [1], [1]] },
-  lPiece: { x: 0, y: 0, shape: [[1, 0], [1,0], [1, 1]] }, 
+  lPiece: { x: 0, y: 0, shape: [[1, 0], [1, 0], [1, 1]] },
   jPiece: { x: 0, y: 0, shape: [[0, 1], [0, 1], [1, 1]] },
-  tPiece: { x: 0, y: 0, shape: [[1,1,1],[0,1,0]]},
+  tPiece: { x: 0, y: 0, shape: [[1, 1, 1], [0, 1, 0]] },
   sPiece: { x: 0, y: 0, shape: [[1, 1, 0], [0, 1, 1]] },
   zPiece: { x: 0, y: 0, shape: [[0, 1, 1], [1, 1, 0]] },
 }
 
 const createField = (): Field[][] => [...new Array(10)].map((): Field[] => [...new Array(20)].map((): Field => 0));
+
+export const dimensions: any = { width: 10, height: 20 };
 
 // SideEffect
 export const newPiece = (): TetrominoPiece => {
@@ -35,28 +35,28 @@ export const newIPiece = (): TetrominoPiece => Object.assign({}, pieces.iPiece);
 export const newOPiece = (): TetrominoPiece => Object.assign({}, pieces.oPiece);
 
 const collisionDetection = (fields: Field[][], piece: TetrominoPiece): boolean => {
-  
+
   let collision = false;
-  
+
   piece.shape.forEach((yFields: Field[], xIndex: number) => {
     yFields.forEach((field: Field, yIndex: number) => {
-      if(field === 1 && fields[piece.x + xIndex ][piece.y + yIndex] === field) {
+      if (field === 1 && piece.x + xIndex < dimensions.width && piece.y + yIndex < dimensions.height && fields[piece.x + xIndex][piece.y + yIndex] === field) {
         collision = true;
         return;
       }
     });
-    if(collision) return;
+    if (collision) return;
   });
 
   return collision;
 }
 
 const placePiece = (field: Field[][], x: number, y: number, shape: Field[][]): Field[][] => {
-  const newField = Object.assign({}, field);
+  const newField = field.concat();
 
   shape.forEach((yFields: Field[], xIndex: number) => {
     yFields.forEach((field: Field, yIndex: number) => {
-      newField[x+ xIndex][y + yIndex] = field;
+      newField[x + xIndex][y + yIndex] = newField[x + xIndex][y + yIndex] || field;
     })
   });
   return newField;
@@ -64,7 +64,7 @@ const placePiece = (field: Field[][], x: number, y: number, shape: Field[][]): F
 
 const movePiece = (field: Field[][], piece: TetrominoPiece, move: Move, nextPiece: TetrominoPiece): Output => {
   const newPos: Move = {
-    x: piece.x + move.x ,
+    x: piece.x + move.x,
     y: piece.y + move.y,
   }
   if (newPos.x < 0) {
@@ -77,12 +77,12 @@ const movePiece = (field: Field[][], piece: TetrominoPiece, move: Move, nextPiec
     }
   }
 
-  if (newPos.x + piece.shape.length > 9) {
+  if (newPos.x + piece.shape.length > dimensions.width) {
     return {
       field,
       piece: {
         ...piece,
-        x: 10 - piece.shape.length,
+        x: dimensions.width - piece.shape.length,
       }
     }
   }
@@ -92,16 +92,18 @@ const movePiece = (field: Field[][], piece: TetrominoPiece, move: Move, nextPiec
     return { field: newField, piece: nextPiece };
   }
 
-  if (newPos.y + piece.shape[0].length > 19) {
-    const newField = placePiece(field, newPos.x, Math.min(18, newPos.y), piece.shape);
+  if (newPos.y + piece.shape[0].length > dimensions.height) {
+    const newField = placePiece(field, newPos.x, Math.min(dimensions.height - piece.shape[0].length), piece.shape);
     return { field: newField, piece: nextPiece };
   }
 
-  return { field, piece: {
-    x: piece.x + move.x,
-    y: piece.y + move.y,
-    shape: [...piece.shape]
-  } };
+  return {
+    field, piece: {
+      x: piece.x + move.x,
+      y: piece.y + move.y,
+      shape: [...piece.shape]
+    }
+  };
 }
 
 const rotatePiece = (field: Field[][], piece: TetrominoPiece): Output => {
@@ -115,9 +117,9 @@ const rotatePiece = (field: Field[][], piece: TetrominoPiece): Output => {
     });
   });
 
-  if (!collisionDetection(field, { ...piece, shape: newShape }) && !(piece.x + newShape.length > 9)){
+  if (!collisionDetection(field, { ...piece, shape: newShape }) && !(piece.x + newShape.length > dimensions.width - 1)) {
     return {
-      field, 
+      field,
       piece: {
         ...piece,
         shape: newShape
@@ -130,6 +132,33 @@ const rotatePiece = (field: Field[][], piece: TetrominoPiece): Output => {
   }
 }
 
+const removeLine = (fields: Field[][], line: number): Field[][] => {
+  return fields.map((xValue: Field[]): Field[] => { 
+    xValue.splice(line, 1); 
+    xValue.unshift(0); 
+    return xValue
+  });
+}
+
+const removeLines = (fields: Field[][]): Field[][] => {
+
+  let fullLine = true;
+
+  for(let y = 0; y < fields[0].length; y++) {
+    for (let x = 0; x < fields.length; x++) {
+      if(fields[x][y] === 0) {
+        fullLine = false;
+      }
+    }
+    if (fullLine) {
+      fields = removeLine([...fields], y)
+    } 
+    fullLine = true;
+  }
+
+  return fields;
+}
+
 // Rules, when you start you get a field and a 2x2 rectangular piece you can move with.
 // When you do a move the piece movies.
 // When the piece hits the bottom, it sticks there a new piece spawns at the top
@@ -137,31 +166,33 @@ const rotatePiece = (field: Field[][], piece: TetrominoPiece): Output => {
 // When a piece hits a stuck piece it sticks to that.
 // It has a long 4x1 piece you can select too.
 // You can rotate pieces.
-
-// TODO:
 // other shapes
 // remove complete lines.
-// make rotation nicer?
+
+// TODO:
+// end game
+// fix rotation
 
 export default (input?: Input): Output => {
-  if (!input){
+  if (!input) {
     return {
       field: createField(),
       piece: newIPiece(),
     }
   }
-  if(input.rotate) {
+  if (input.rotate) {
     const { field, piece } = rotatePiece(input.field, input.piece);
     return {
       field,
       piece
     }
   }
-  if(input.move) {
-    const { field, piece } = movePiece(input.field, input.piece, input.move!, input.nextPiece);
+  if (input.move) {
+    const movePieceOutput = movePiece(input.field, input.piece, input.move!, input.nextPiece);
+    const field = removeLines(movePieceOutput.field);
     return {
       field,
-      piece
+      piece: movePieceOutput.piece
     }
   }
   const { field, piece } = input;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,73 @@
-export default (): void => {
+import tetris, { dimensions } from './functional/tetris';
+import { Output, newPiece } from './functional/tetris';
+import { Field } from './models/ticTacToe.model';
+import { TetrominoPiece } from './models/tetris.model';
 
+const rectSize = 25;
+
+const setup = (): HTMLCanvasElement => {
   const canvas = document.createElement('canvas');
 
   canvas.id = "CursorLayer";
-  canvas.width = 1224;
-  canvas.height = 768;
+  canvas.width = dimensions.width * rectSize;
+  canvas.height = dimensions.height * rectSize;
   canvas.style.zIndex = '8';
   canvas.style.position = "absolute";
   canvas.style.border = "1px solid";
 
   document.body.appendChild(canvas);
-
-  canvas.textContent = "Hello World";
+  return canvas;
 }
+
+const canvas = setup();
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+let state: Output;
+state = tetris();
+let nextPiece: TetrominoPiece;
+
+const drawField = (field: Field[][]): void => {
+  field.forEach((xFields: Field[], xIndex: number) => xFields.map((field: Field, yIndex: number) => {
+    ctx.fillStyle = field === 1 ? "#00FF00" : "#FF0000";
+    ctx.fillRect(rectSize*xIndex, rectSize*yIndex, rectSize, rectSize);
+  }));
+}
+
+const drawPiece = (piece: TetrominoPiece): void => {
+  (piece.shape as Field[][]).forEach((xFields: Field[], xIndex: number) => xFields.map((field: Field, yIndex: number) => {
+    ctx.fillStyle = "#000000";
+    if( field === 1) {
+      ctx.fillRect(rectSize * (piece.x + xIndex), rectSize * (piece.y + yIndex), rectSize, rectSize);
+    }
+  }));
+}
+
+setInterval(() => {
+  nextPiece = newPiece();
+  state = tetris({ ...state, move: { x: 0, y: 1 }, nextPiece});
+  drawField(state.field as Field[][]);
+  drawPiece(state.piece);
+}, 500);
+
+document.addEventListener('keydown', (event) => {
+  switch (event.keyCode) {
+    case 37:
+      // left
+      state = tetris({ ...state, move: { x: -1, y: 0 }, nextPiece })
+      break;
+    case 38:
+      // up
+      state = tetris({ ...state, rotate: true, nextPiece })
+      break;
+    case 39:
+      // right
+      state = tetris({ ...state, move: { x: 1, y: 0}, nextPiece })
+      break;
+    case 40:
+      // down
+      state = tetris({ ...state, move: { x: 0, y: 1 }, nextPiece })
+      break;
+  }
+
+  drawField(state.field as Field[][]);
+  drawPiece(state.piece);
+}, false);
